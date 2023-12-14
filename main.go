6b99fc5be2491f13c4673beb32d3599dc6270ce3
Hroman9n <s.vasin99@mail.ru>
2023-12-14 22:47:00 +0300

    First working solution

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Структура альбомов
+// type album struct {
+// 	ID     string  `json:"id"`
+// 	Title  string  `json:"title"`
+// 	Artist string  `json:"artist"`
+// 	Price  float64 `json:"price"`
+// }
+
+// TODO: Тестовые данные
+// var albums = []album{
+// 	{ID: "1", Title: "IGOR", Artist: "Tyler, The creator", Price: 3.99},
+// 	{ID: "2", Title: "Ethernal blue", Artist: "Spiritbox", Price: 2.5},
+// 	{ID: "3", Title: "Madvillain", Artist: "MF DOOM", Price: 5},
+// }
+
+// func getAlbums() {}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Hello!\n")
+}
+
+// This handler does something a little more sophisticated by reading all the HTTP request headers and echoing them into the response body.
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8090", nil)
+}
